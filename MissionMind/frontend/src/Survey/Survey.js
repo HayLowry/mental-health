d9dcdcc6e24167abaa0d90a9f7b52be92c9563d5
gregeva <evang0911@gmail.com>
2023-12-08 18:55:43 -0700

    updated the survey for the a/b testing. need to ge t the submmit button to work

--- MissionMind/frontend/src/Survey/Survey.js
+++ MissionMind/frontend/src/Survey/Survey.js
@@ -1,22 +1,186 @@
-import React from "react";
-import SubmissionForm from "../components/SubmissionForm"
+// import React from "react";
+// import SubmissionForm from "../components/SubmissionForm"
 
-class Survey extends React.Component {
-  render() {
-    return (
+// class Survey extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <br /><br />
+//         <h1>Daily Survey</h1>
+//         <br /><br />
+
+//         <SubmissionForm
+//           resetState={this.props.resetState}
+//           submission={this.props.submission}
+//         />
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default Survey;
+
+import React, { useState } from "react";
+import styles from "./Survey.module.css";
+import { Link } from "react-router-dom";
+import SubmissionForm from "../components/SubmissionForm";
+
+function Survey() {
+  // State to store the answers for each question
+  const [answers, setAnswers] = useState({
+    question1: 3, // Default value
+    question2: 3,
+    question3: 3,
+    question4: 3,
+    question5: 3,
+  });
+
+  // Handle slider value change
+  const handleSliderChange = (question, value) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [question]: value,
+    }));
+  };
+
+  // Handle button click
+  const handleButtonClick = (question, value) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [question]: value,
+    }));
+  };
+
+  const smileyFaces = ["😄", "😊", "😐", "😕", "😭"];
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can do something with the answers, like sending them to a server
+    console.log("Survey Answers:", answers);
+  };
+
+  return (
+    <>
       <div>
-        <br /><br />
-        <h1>Daily Survey</h1>
-        <br /><br />
-
-        <SubmissionForm
-          resetState={this.props.resetState}
-          submission={this.props.submission}
-        />
-
+        <h1>Survey</h1>
       </div>
-    );
-  }
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="question1">Question 1</label>
+          <br />
+          {smileyFaces.map((face, index) => (
+            <button
+              key={index}
+              type="button"
+              onClick={() => handleButtonClick("question1", index + 1)}
+              className={styles.button} // Add your styling class
+              style={{
+                background:
+                  answers.question1 === index + 1 ? "lightblue" : "white",
+              }}
+            >
+              {face}
+            </button>
+          ))}
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="question2">Question 2</label>
+          <br />
+          {smileyFaces.map((face, index) => (
+            <button
+              key={index}
+              type="button"
+              onClick={() => handleButtonClick("question2", index + 1)}
+              className={styles.button} // Add your styling class
+              style={{
+                background:
+                  answers.question2 === index + 1 ? "lightblue" : "white",
+              }}
+            >
+              {face}
+            </button>
+          ))}
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="question3">Question 3</label>
+          <br />
+          {smileyFaces.map((face, index) => (
+            <button
+              key={index}
+              type="button"
+              onClick={() => handleButtonClick("question3", index + 1)}
+              className={styles.button} // Add your styling class
+              style={{
+                background:
+                  answers.question3 === index + 1 ? "lightblue" : "white",
+              }}
+            >
+              {face}
+            </button>
+          ))}
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="question4">Question 4</label>
+          <br />
+          {smileyFaces.map((face, index) => (
+            <button
+              key={index}
+              type="button"
+              onClick={() => handleButtonClick("question4", index + 1)}
+              className={styles.button} // Add your styling class
+              style={{
+                background:
+                  answers.question4 === index + 1 ? "lightblue" : "white",
+              }}
+            >
+              {face}
+            </button>
+          ))}
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="question5">Question 5</label>
+          <br />
+          {smileyFaces.map((face, index) => (
+            <button
+              key={index}
+              type="button"
+              onClick={() => handleButtonClick("question5", index + 1)}
+              className={styles.button} // Add your styling class
+              style={{
+                background:
+                  answers.question5 === index + 1 ? "lightblue" : "white",
+              }}
+            >
+              {face}
+            </button>
+          ))}
+        </div>
+        {/* Repeat similar blocks for other questions... */}
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <div>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Survey;
